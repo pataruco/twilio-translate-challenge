69664f67d31a9b1885bbd4dbbec6fd9338031b28
Pedro Martin <pataruco@gmail.com>
2020-10-21 16:05:23 +0100

    Add twilio credentials

--- controller/message.js
+++ controller/message.js
@@ -1,7 +1,9 @@
+import { TWILIO_SID, TWILIO_TOKEN } from '../index.js';
+
 export const sendMessage = async (request, response) => {
   const { body } = request;
 
-  console.log({ body });
+  console.log({ body, TWILIO_SID, TWILIO_TOKEN });
 
   return response.status(200).send({
     message: 'Translation successful. Message sent.',
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from './lib/logger.js';
+import dotenv from 'dotenv';
+
 import messageRouter from './routes/message.js';
 // midlewares
 import httpLoggerMiddleware from './middleware/logger-middleware.js';
 import jsonResponseMiddleware from './middleware/json-response.js';
 
+dotenv.config();
 const HOST = '127.0.0.1';
 const PORT = 5000;
+export const { TWILIO_SID, TWILIO_TOKEN } = process.env;
 
 const server = express();
 // midlewares
